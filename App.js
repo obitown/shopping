f666f4333da5912aaf392640bdad2df5ed5177bf
obitown <92471740+obitown@users.noreply.github.com>
2022-04-26 12:47:03 -0700

    update

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// import * as firebase from './firebase';
+// import 'firebase/firestore'
+
+const firebase = require('firebase')
+require('firebase/firestore')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontWeight: '900' }}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#93D8FF',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
 });
