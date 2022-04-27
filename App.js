d8ee626be2491fe64e27c4c474ab342cd9ea0b9a
obitown <92471740+obitown@users.noreply.github.com>
2022-04-27 10:57:23 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-// import * as firebase from './firebase';
-// import 'firebase/firestore'
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 const firebase = require('firebase')
 require('firebase/firestore')
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={{ fontWeight: '900' }}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      lists: []
+    }
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyAvew1oYkmw-MvJfNU7pLJMsxU0_1aOJHc",
+      authDomain: "test-604cd.firebaseapp.com",
+      projectId: "test-604cd",
+      storageBucket: "test-604cd.appspot.com",
+      messagingSenderId: "806267742453",
+      appId: "1:806267742453:web:a062c00f2e3b629d513dbc"
+    }
+
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig)
+    }
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{ fontWeight: '900' }}>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
